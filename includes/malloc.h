2ccd24a7d97d70635e7429b8f96a04f87ac89779
Bryan Maruani <bryan.maruani9@gmail.com>
2015-02-17 12:40:46 +0100

    sfd

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/12 19:32:17 by mbryan            #+#    #+#             */
-/*   Updated: 2015/02/12 19:56:48 by mbryan           ###   ########.fr       */
+/*   Updated: 2015/02/17 11:55:59 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 #	define MALLOC_H
 
 
- #include "libft.h"
- #include <sys/mman.h>
+#include "libft.h"
+#include <sys/mman.h>
 #include <stdlib.h>
+
+typedef struct		s_block
+{
+	size_t			size;
+	int				state;
+	int				debug;
+	struct s_block	*next;
+}					t_block;
 
 #	endif
--- main.c
+++ main.c
@@ -6,21 +6,62 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/12 19:30:52 by mbryan            #+#    #+#             */
-/*   Updated: 2015/02/12 20:00:28 by mbryan           ###   ########.fr       */
+/*   Updated: 2015/02/17 12:40:18 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
+#include <stdio.h>
 
+t_block		*update_struct(t_block *block)
+{
+	return (block);
+}
+
+t_block		*ask_for_space(t_block *last, size_t size)
+{
+	int		all_sz;
+	t_block *block;
+
+	all_sz = size + sizeof(t_block);
+	printf("size_all : %d\n",all_sz );
+	block = mmap(0, all_sz, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	if (block == MAP_FAILED)
+		return (NULL);
+	if (last)
+		last->next = block;
+	block->size = size;
+	block->next = NULL;
+	block->state = 0;
+	block->debug = 0x789456;
+	return (block);
+}
 
 void	*ft_malloc(size_t size)
 {
-	void	*ret;
+	t_block *block;
+	t_block *last;
+	t_block *first;
 
-	ret = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	if (ret == MAP_FAILED)
+	first = NULL;
+	if (size <= 0)
 		return (NULL);
-	return (ret);
+	if (!first)
+	{
+		block = ask_for_space(NULL, size);
+		if (!block)
+			return (NULL);
+		first = block;
+	}
+	else 
+	{
+		last = first;
+		block = ask_for_space(last, size);
+		if (!block)
+			return (NULL);
+	}
+	printf("size_malloc : %d\n",(unsigned int)block->size );
+	return (block + 1);
 }
 
 int main(int argc, char **argv)
